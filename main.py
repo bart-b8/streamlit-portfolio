ff24df8b625de31aa1beab55ab3a48f4bbff624b
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2022-11-29 15:04:02 +0100

    Add column and data to page

--- main.py
+++ main.py
@@ -21,11 +21,18 @@ st.write(content2)
 
 df = pd.read_csv("data.csv", sep=';')
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([2,0.5,2])
 
 with col3:
     for index, row in df[::2].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/"+row['image'])
+        st.write("[Source Code]({})".format(row['url']))
 with col4:
     for index, row in df[1::2].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/"+row['image'])
+        st.write("[Source Code]({})".format(row['url']))
+        
