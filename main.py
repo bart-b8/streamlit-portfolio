b4bc421aa3698799fe737e5eb4c975ab8a27465b
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2022-11-29 14:32:42 +0100

    Add descriptions of main area of the page

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ with col2:
     """
     st.info(content)
 
+content2 = """
+        Below you can find some of the apps I have build in Python.  Feel free to contact me!
+        """
+st.text(content2)
+
